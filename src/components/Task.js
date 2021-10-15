0e384aaf7a9b247830083821f699ab77e0dcf8d0
Marco Antônio Furtado <marcofurt@gmail.com>
2021-10-15 16:44:01 -0300

    criando um botão de deletar e algumas divs para maior responsividade

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,12 +1,18 @@
 import '../styles/task.css'
 
-function Task({ task, handleTaskClick }){
+function Task({ task, handleTaskClick, handleTaskDeletion }){
     return(
-        <div className="task-container"  onClick={() => handleTaskClick(task.id)}
+        <div className="task-container"  
         style={task.completed ? { borderLeft: "6px solid chartreuse" }: {}}
         >
+            <div className="task-title" onClick={() => handleTaskClick(task.id)}>
                 {task.title}
-            
+            </div>    
+
+            <div>
+                <button className="btn-deletar" onClick={() => handleTaskDeletion(task.id)}>x</button>
+            </div>
+
         </div>
     )
 }
