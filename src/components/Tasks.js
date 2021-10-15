35c4c16aedb9d0ed6979349efcd1bc805ea98d86
Marco Antônio Furtado <marcofurt@gmail.com>
2021-10-15 16:42:51 -0300

    recebendo a função de deletar e passando ela adiante para "Task"

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,9 +1,9 @@
 import Task from './Task'
 
-function Tasks({ tasks, handleTaskClick }) {
+function Tasks({ tasks, handleTaskClick, handleTaskDeletion }) {
     return (
       <div className="Tasks">
-        {tasks.map((task, id) => (<Task handleTaskClick={handleTaskClick} key={id} task={task} />
+        {tasks.map((task, id) => (<Task handleTaskClick={handleTaskClick} handleTaskDeletion={handleTaskDeletion} key={id} task={task} />
         ))}
       </div>
     );
