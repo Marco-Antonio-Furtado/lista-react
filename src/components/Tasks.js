2564cdfeb3a204a2dc801992f4e4365a38c219d4
Marco Antônio Furtado <marcofurt@gmail.com>
2021-10-18 16:48:22 -0300

    Update Tasks.js

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -3,7 +3,11 @@ import Task from './Task'
 function Tasks({ tasks, handleTaskClick, handleTaskDeletion }) {
     return (
       <div className="Tasks">
-        {tasks.map((task, id) => (<Task handleTaskClick={handleTaskClick} handleTaskDeletion={handleTaskDeletion} key={id} task={task} />
+        {tasks.map((task, id) => 
+        (<Task handleTaskClick={handleTaskClick} 
+          handleTaskDeletion={handleTaskDeletion} 
+          key={id} 
+          task={task} />
         ))}
       </div>
     );
